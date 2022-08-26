64b1a57117fc65e4420585de138e9e1a04df6092
Berzan Özejder <berzanozejder@gmail.com>
2022-08-26 19:32:24 +0300

    Started PDF Generation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import './style.css';
-console.log("Hello World!");
+import printJS from "print-js";
+console.log( 'HELLO' );
+
+function printTest ()
+{
+  printJS( {
+    printable: "page",
+    type: "html",
+    css: [
+      "/style.css",
+      "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400;1,500&display=swap"
+    ],
+    scanStyles: false
+  } );
+}
+
+document.getElementById( "printBtn" ).addEventListener( "click", printTest );
